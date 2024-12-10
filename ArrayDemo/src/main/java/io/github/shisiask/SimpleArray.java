3e14d1681139f9f9362ca5a4f8fccedb222884ba
5hisia <edu.osteen@gmail.com>
2024-12-10 14:31:59 -0500

    introduction to boxing

---
+++ ArrayDemo/src/main/java/io/github/shisiask/SimpleArray.java
@@ -0,0 +1,20 @@
+package io.github.shisiask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleArray {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(50);
+
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+//        for(int i = 0; i < list.size(); i++){
+//            System.out.println(list.get(i));
+//        }
+
+    }
+}
